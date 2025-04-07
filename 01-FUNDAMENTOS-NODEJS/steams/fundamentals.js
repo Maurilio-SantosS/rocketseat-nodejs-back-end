491a41e1c027d3a26310535b01ac6c92aaff0a1c
Maurilio-SantosS <msscgenio@gmail.com>
2025-04-07 18:23:12 -0300

    Treinando e criando steams

--- 01-FUNDAMENTOS-NODEJS/steams/fundamentals.js
+++ 01-FUNDAMENTOS-NODEJS/steams/fundamentals.js
@@ -11,3 +11,47 @@
 // 10mb/s -> 10.000
 
 // Readable Steams / Writable Steams
+
+
+// process.stdin
+//     .pipe(process.stdout)
+
+import { Readable, Writeable, Transform } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++
+
+        setTimeout(() => {
+            if(i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+    
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+class InverseNumberSteam extends Transform {
+    _transform() {
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writeable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+
+new OneToHundredStream()
+    .pipe(new InverseNumberSteam())
+    .pipe(new MultiplyByTenStream())
