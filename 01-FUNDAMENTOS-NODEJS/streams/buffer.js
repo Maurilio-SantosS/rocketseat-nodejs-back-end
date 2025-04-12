d452d656f5bd18bf0b4d350bd50870883662b16e
Maurilio-SantosS <msscgenio@gmail.com>
2025-04-12 12:10:09 -0300

    Aprendendo sobre Buffers

---
+++ 01-FUNDAMENTOS-NODEJS/streams/buffer.js
@@ -0,0 +1,8 @@
+// Representação de um espaço da mémoria do computador usada para transmitir dados rapidamente (formato binário hexadecimal)
+const buf = Buffer.from("hello")
+
+// Dado em hexadecimal
+console.log(buf)
+
+// Dado em decimal
+console.log(buf.toJSON())
--- 01-FUNDAMENTOS-NODEJS/streams/stream-http-server.js
+++ 01-FUNDAMENTOS-NODEJS/streams/stream-http-server.js
@@ -15,10 +15,24 @@ class InverseNumberSteam extends Transform {
 // req => readable stream
 // res => writable stream
 
-const server = http.createServer((req, res) => {
-    return req
-    .pipe(new InverseNumberSteam())
-    .pipe(res)
+const server = http.createServer(async (req, res) => {
+    // ESCREVE OS DADOS DEPOIS QUE ELE É LIDO POR INTEIRO
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString
+
+    console.log(fullStreamContent)
+
+    return res.end(fullStreamContent)
+
+    // LENDO E ESCREVENDO OS DADOS EM PEDAÇOS
+    // return req
+    // .pipe(new InverseNumberSteam())
+    // .pipe(res)
 })
 
 server.listen(3334)
